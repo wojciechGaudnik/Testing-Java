c4e9b3459b8222081060371d08bd201558c2ca6f
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-04-21 22:40:31 +0200

    spring context tests

---
+++ tb2g-testing-spring-with-context/src/test/java/org/springframework/samples/petclinic/sfg/YannyConfig.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class YannyConfig {
+
+	@Bean
+	YannyWordProducer yannyWordProducer(){
+		return new YannyWordProducer();
+	}
+}
+   
