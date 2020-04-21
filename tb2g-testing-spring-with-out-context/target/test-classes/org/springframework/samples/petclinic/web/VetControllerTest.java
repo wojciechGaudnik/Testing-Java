d6fab1db87fb845459c1832ae852116d21917037
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-04-21 13:09:44 +0200

    init spring context testx

--- tb2g-testing-spring-with-out-context/target/test-classes/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ tb2g-testing-spring-with-out-context/target/test-classes/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -7,14 +7,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
+import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTest {
@@ -41,9 +47,28 @@ class VetControllerTest {
 
 	@Test
 	void showVetList() {
+		//when
+		String view = vetController.showVetList(model);
+
+		//then
+		then(clinicService).should().findVets();
+		then(model).should().put(anyString(), any());
+		assertThat("vets/VetList").isEqualToIgnoringCase(view);
+		//verify
+
+
+
 	}
 
 	@Test
 	void showResourcesVetList() {
+		//when
+		Vets vets = vetController.showResourcesVetList();
+
+		//then
+		then(clinicService).should().findVets();
+		assertThat(vets.getVetList()).hasSize(1);
+//		assertThat(vetsList.g)
+
 	}
 }
