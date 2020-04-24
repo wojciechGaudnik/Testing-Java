c6eb65785acb6a4e7823711189eed676365e4442
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-04-24 23:07:47 +0200

    init MVC Rest tests

--- tb2g-sfg-brewery/src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
+++ tb2g-sfg-brewery/src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
@@ -2,12 +2,16 @@ package guru.springframework.brewery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class TsbbSfgBreweryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TsbbSfgBreweryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TsbbSfgBreweryApplication.class, args);
+    }
 
 }
--- tb2g-sfg-brewery/src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ tb2g-sfg-brewery/src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TsbbSfgBreweryApplicationTests {
+public class TsbbSfgBreweryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
