ca72b07b234a715538e29b1d7368fb57111c8072
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-04-26 23:48:29 +0200

    WebMVC and IT test

--- tb2g-sfg-brewery/target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerOrderMapperImpl.java
+++ tb2g-sfg-brewery/target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerOrderMapperImpl.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-24T23:07:07+0200",
+    date = "2020-04-25T13:56:20+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
